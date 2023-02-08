8e6828b3d4f7422eed247d826317e402a0130ef6
sema301183 <123240888+sema301183@users.noreply.github.com>
2023-02-08 23:12:20 +0300

    Update and rename src/Main.java to Src/Main.java

---
+++ Src/Main.java
@@ -0,0 +1,18 @@
+Основной  класс  public {
+main  аннулирует  статические  общедоступные (String[] аргументы) {
+
+eaters int = 5; // сколько людей будут есть
+
+water int = 3000; // миллилитров воды
+potatoes int = 5; // картофелин
+chicken int = 6; // куриных бёдер
+spices int = 10; // ложек специй
+
+System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров(а) воды");
+        System.out.println((картофель / едоки) + " картофелин(а/ы)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ка) специй");
+
+    }
+}
